62836c2315dd66d667c20319e00ce2ee378e3f9e
mjs030507-beep <mjs030507-beep@users.noreply.github.com>
2026-08-18 02:10:23 +0900

    fix: load GA config independently from database

--- netlify/functions/public-config.js
+++ netlify/functions/public-config.js
@@ -1,6 +1,9 @@
-import { json } from "./_shared.js";
-
-export default async () => json({
+export default async () => new Response(JSON.stringify({
   appVersion: process.env.APP_VERSION || "v2",
   gaMeasurementId: process.env.GA_MEASUREMENT_ID || "",
+}), {
+  headers: {
+    "Content-Type": "application/json; charset=utf-8",
+    "Cache-Control": "no-store",
+  },
 });
